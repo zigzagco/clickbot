3a64b8a529b133583c5016e9e4167db42876129d
gleb <g1397209@gmail.com>
2023-01-19 19:36:27 +0300

    -v1

--- index.js
+++ index.js
@@ -2,57 +2,25 @@ const puppeteer = require('puppeteer-extra')
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 const creds = require('./proxybot-374920-634195a0e3b1.json');
-const fetch = require("node-fetch");
 const https = require('https'); // or 'https' for https:// URLs
 const fs = require('fs');
 const { executablePath } = require('puppeteer');
 puppeteer.use(StealthPlugin())
 const paths = './extentions/eppiocemhmnlbhjplcgkofciiegomcon/2.5.10_0';
 const paths2 = './extentions/majdfhpaihoncoakbjgbdhglocklcgno/2.6.0_0';
+const paths3 = './extentions/lpejglcfpkpbjhmnnmpmmlpblkcmdgmi/1.9.3_0';
         try {
             (async () => {
-                for (let i=0;i<5;i++){
-                    await startBrowser()
+                for (let i=0;i<10;i++){
+                    await startBrowserPath2()
                     console.log('ok '+i)
                 }
             })();
         }catch (e){
             console.log('vpn strted')
         }
-        //const page = await browser.newPage()
-        //await page.goto('https://spys.me/socks.txt', { waitUntil: 'networkidle2' });
 
-        //https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.json
-        //const str = 'http://' + pathstr[i].ip + ':' + pathstr[i].port
-        //console.log(str)
-        /* try {
-             //await useProxy(page, str);
-             //await page.goto('https://api.ipify.org/')
-             //await page.goto('chrome-extension://');
-             //await page.screenshot({path: 'msedge-extension.png'});
-             await page.goto('chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/popup/index.html#/main');
-             await clickSelector(page,'body > div > div > div.simple_layout__body > div > div > div.controls > button.button_pink.agreement_agree')
-             await page.waitForTimeout(3000)
-             await clickSelector(page,'body > div > div > div.main_layout__body > div.timer-box > div > div')
-             await page.waitForTimeout(300000000)
-             console.log('ok')
-             await page.close()
-         }catch (e){
-             console.log('error')
-             await page.close()
-         }*/
-
-/*async function clearInput(page,selector) {
-    await page.waitForSelector(selector);
-    let input = await page.$(selector);
-    await input.click({clickCount: 3});
-    await input.press('Backspace');
-}
-async function clickInput(page,selector,text) {
-    await page.waitForSelector(selector);
-    await page.type(selector,text);
-}*/
-async function startBrowser(){
+async function startBrowserPath1(){
     const browser = await puppeteer.launch({
         headless:false,
         executablePath: executablePath(),
@@ -64,9 +32,6 @@ async function startBrowser(){
     const [mainPage] = await browser.pages();
     try {
         console.log('script started')
-        //await page.goto('https://api.ipify.org/')
-        //await page.screenshot({path: 'msedge-extension.png'});
-        //await page.goto('chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/popup/index.html#/main');
         await mainPage.waitForTimeout(5000)
         await mainPage.goto('chrome-extension://majdfhpaihoncoakbjgbdhglocklcgno/html/foreground.html',);
         await mainPage.waitForTimeout(5000)
@@ -76,28 +41,29 @@ async function startBrowser(){
         await mainPage.waitForTimeout(3000)
         await mainPage.reload()
         await clickSelector(mainPage,'#content > div.current-region > div > div.current-region-upper-block')
-        switch ('ru'){
-            case 'fr':{
+        const randomint=await randomInteger(0,5)
+        switch (randomint){
+            case 0:{
                 await clickSelector(mainPage,'#region-list > div:nth-child(2) > div.radio.off')
                 break;
             }
-            case 'ne':{
+            case 1:{
                 break;
             }
-            case 'ru':{
+            case 2:{
                 await clickSelector(mainPage,'#region-list > div:nth-child(4) > div.radio.off')
                 break;
             }
-            case 'si':{
+            case 3:{
                 await clickSelector(mainPage,'#region-list > div:nth-child(5)')
                 await clickSelector(mainPage,'#region-list > div:nth-child(5) > div.rah-static.rah-static--height-specific > div > div:nth-child(1) > div.radio.off')
                 break;
             }
-            case 'uk':{
+            case 4:{
                 await clickSelector(mainPage,'#region-list > div:nth-child(6) > div.radio.off')
                 break;
             }
-            case 'us':{
+            case 5:{
                 await clickSelector(mainPage,'#region-list > div:nth-child(7)')
                 await clickSelector(mainPage,'#region-list > div:nth-child(7) > div.rah-static.rah-static--height-specific > div > div:nth-child(1)')
                 break;
@@ -111,7 +77,7 @@ async function startBrowser(){
             return document.querySelector('body > pre').innerText
         }).catch(e => console.dir(e));
         console.log(ipchek)
-        await setData(ipchek)
+        await setData(ipchek,randomint)
         await mainPage.close()
         await browser.close()
     }catch (e){
@@ -120,17 +86,74 @@ async function startBrowser(){
         await browser.close()
     }
 }
+async function startBrowserPath2(){
+    const browser = await puppeteer.launch({
+        headless:false,
+        executablePath: executablePath(),
+        args: [
+            '--no-sandbox',
+            `--load-extension=${paths3}`
+        ]
+    })
+    const [mainPage] = await browser.pages();
+    try {
+        console.log('script started')
+        await mainPage.waitForTimeout(5000)
+        await mainPage.goto('chrome-extension://lpejglcfpkpbjhmnnmpmmlpblkcmdgmi/html/popup.html',);
+        await mainPage.waitForTimeout(5000)
+        await mainPage.reload()
+        await clickSelector(mainPage,'body > div.current-location-wrapper.w-100.d-flex.justify-content-center > div')
+        const randomint=await randomInteger(0,20)
+        await clickSelector(mainPage,'body > div.location-list-wrapper.w-100.position-absolute.flex-column.align-items-center.d-flex > div.location-list.w-100 > div:nth-child('+randomint+')')
+        await mainPage.waitForTimeout(10000)
+        const status=await mainPage.evaluate(() => {
+            return document.querySelector('#status').innerText
+        }).catch(e => console.dir(e));
+        if (status==='Not connected'){
+            await mainPage.close()
+            await browser.close()
+        }else {
+            await mainPage.goto('https://api.ipify.org')
+            const ipchek=await mainPage.evaluate(() => {
+                return document.querySelector('body > pre').innerText
+            }).catch(e => console.dir(e));
+            console.log(ipchek)
+            await setData(ipchek,'Undefined')
+        }
+        await mainPage.close()
+        await browser.close()
+    }catch (e){
+        console.log(e)
+        await browser.close()
+    }
+}
 async function clickSelector(page,selector) {
     await page.waitForSelector(selector)
     await page.click(selector)
 }
-async function setData(ip) {
+async function randomInteger(min, max) {
+    // получить случайное число от (min-0.5) до (max+0.5)
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+}
+async function setData(ip,countryCodeInt) {
     const doc = new GoogleSpreadsheet('1dz-VES3gSjfpEgOAnfnIya_JJEvu1ehoscr0nMEnL4Y');
     await doc.useServiceAccountAuth(creds);
     await doc.loadInfo()
     const sheet = doc.sheetsByIndex[0];
-    await sheet.addRow({ Ip: ip, Time: get_time(),Date:get_day() });
+    await sheet.addRow({ Ip: ip, Time: get_time(),Date:get_day(),Country: switchCountrycode(countryCodeInt)});
 
+}
+function switchCountrycode(countryCode){
+    switch (countryCode){
+        case 0:return 'FR'
+        case 1:return 'NE'
+        case 2:return 'RU'
+        case 3:return 'SI'
+        case 4:return 'UK'
+        case 5:return 'US'
+        case 'Undefined':return 'Undefined'
+    }
 }
 function get_time(){
     return (new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds())
