eecc2352eb9441262234a7a954fc7c7bf72f72d1
gleb <g1397209@gmail.com>
2023-04-05 23:01:15 +0300

    -v2

--- newtest.js
+++ newtest.js
@@ -59,7 +59,7 @@ const task = (msg,text,docName,startTime,endTime,callback) => {
                 //await mainPage.setUserAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1')
                 console.log('ok ' + i)
             }catch (e){
-                console.log('vpn strted')
+                console.log('start browser error'+e)
             }
             ////////
 
